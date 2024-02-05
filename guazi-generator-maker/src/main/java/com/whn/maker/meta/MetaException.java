5229016ff5ed69e2a93780dbbe89133e81c0d444
whn <1874695345@qq.com>
2024-02-05 22:28:15 +0800

    Optimized some generators' codes

---
+++ guazi-generator-maker/src/main/java/com/whn/maker/meta/MetaException.java
@@ -0,0 +1,14 @@
+package com.whn.maker.meta;
+
+/**
+ *  元信息异常类
+ */
+public class MetaException extends RuntimeException{
+    public MetaException(String message) {
+        super(message);
+    }
+
+    public MetaException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
---
+++ guazi-generator-maker/src/main/java/com/whn/maker/meta/eunm/FileGenerateTypeEnum.java
@@ -0,0 +1,27 @@
+package com.whn.maker.meta.eunm;
+
+/**
+ * 文件生成类型枚举类
+ */
+public enum FileGenerateTypeEnum {
+
+    STATIC("静态", "static"),
+    DYNAMIC("动态", "dynamic");
+
+    private final String text;
+
+    private final String value;
+
+    FileGenerateTypeEnum(String text, String value) {
+        this.text = text;
+        this.value = value;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
